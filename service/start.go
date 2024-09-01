a87a68026d7822ee005530220414440282fc15e7
freeman7728 <1357880399@qq.com>
2024-09-01 19:58:54 +0800

    实现了消息的广播

--- service/start.go
+++ service/start.go
@@ -5,11 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
 )
 
 func (manager *ClientManager) Start() {
 	for {
-		fmt.Println("---------监听管道通信---------")
+		fmt.Println("---------正在监听管道通信---------")
 		select {
 		case conn := <-manager.Register:
 			fmt.Printf("有新的连接:%v\n", conn.ID)
@@ -20,7 +21,66 @@ func (manager *ClientManager) Start() {
 			}
 			msg, _ := json.Marshal(replyMsg)
 			_ = conn.Socket.WriteMessage(websocket.TextMessage, msg)
-
+		case conn := <-manager.Unregister: //断开连接
+			log.Printf("连接失败:%v\n", conn.ID)
+			if _, ok := Manager.Clients[conn.ID]; ok {
+				replyMsg := &ReplyMsg{
+					Code:    e.WebsocketEnd,
+					Content: "连接已断开",
+				}
+				msg, _ := json.Marshal(replyMsg)
+				_ = conn.Socket.WriteMessage(websocket.TextMessage, msg)
+				close(conn.Send)
+				delete(Manager.Clients, conn.ID)
+			}
+		case broadcast := <-manager.Broadcast:
+			message := broadcast.Message
+			sendId := broadcast.Client.SendId
+			flag := false
+			for id, conn := range Manager.Clients {
+				if id != sendId {
+					continue
+				}
+				select {
+				case conn.Send <- message:
+					flag = true
+				default:
+					close(conn.Send)
+					delete(Manager.Clients, sendId)
+				}
+			}
+			//id := broadcast.Client.ID
+			//在线
+			if flag {
+				log.Println("对方在线")
+				replyMsg := &ReplyMsg{
+					Code:    e.WebsocketOnlineReply,
+					Content: "对方在线",
+				}
+				msg, _ := json.Marshal(replyMsg)
+				_ = broadcast.Client.Socket.WriteMessage(websocket.TextMessage, msg)
+				//TODO 聊天记录插入数据库
+				//err = InsertMsg(conf.MongoDBName, id, string(message), 1, int64(3*month))
+				//if err != nil {
+				//	fmt.Println("InsertOneMsg Err", err)
+				//}
+			} else {
+				log.Println("对方不在线")
+				replyMsg := ReplyMsg{
+					Code:    e.WebsocketOfflineReply,
+					Content: "对方不在线应答",
+				}
+				msg, _ := json.Marshal(replyMsg)
+				_ = broadcast.Client.Socket.WriteMessage(websocket.TextMessage, msg)
+				//err = InsertMsg(conf.MongoDBName, id, string(message), 0, int64(3*month))
+				//if err != nil {
+				//	fmt.Println("InsertOneMsg Err", err)
+				//}
+			}
+			//不在线
 		}
+
+		//TODO 消息的广播/传递
+
 	}
 }
