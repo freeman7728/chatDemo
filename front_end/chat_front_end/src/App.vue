1f940d024a0ed6229c086bb056f12b48b3829aac
freeman7728 <1357880399@qq.com>
2024-09-12 20:36:20 +0800

    添加了前端文件

--- front_end/chat_front_end/src/App.vue
+++ front_end/chat_front_end/src/App.vue
@@ -1,26 +1,19 @@
+<!--
+ * @Description: 
+ * @author: freeman7728
+ * @Date: 2024-09-12 18:48:46
+ * @LastEditTime: 2024-09-12 19:50:12
+ * @LastEditors: freeman7728
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld></HelloWorld>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
+  name:'app',
+  components:{
     HelloWorld
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
