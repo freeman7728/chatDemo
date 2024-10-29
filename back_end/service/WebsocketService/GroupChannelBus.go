1d5ece2e10f66b14f9512a0e9d43493ee01a582d
freeman7728 <1357880399@qq.com>
2024-10-29 14:22:05 +0800

    群聊管道

--- back_end/service/WebsocketService/GroupChannelBus.go
+++ back_end/service/WebsocketService/GroupChannelBus.go
@@ -1,11 +1,20 @@
 package WebsocketService
 
-//func () Start() {
-//	for {
-//		fmt.Println("---------正在监听用户管道通信---------")
-//		select {
-//
-//		}
-//	}
-//
-//}
+import (
+	"fmt"
+)
+
+func (manager *GroupClientManager) Start() {
+	for {
+		fmt.Println("---------正在监听创建群聊管道通信---------")
+		select {
+		case gid := <-manager.Register:
+			client := &GroupClient{
+				GroupId:       gid,
+				Broadcast:     make(chan *Broadcast),
+				OnlineUserMap: make(map[string]ConnGroupClient),
+			}
+			manager.Clients[gid] = client
+		}
+	}
+}
